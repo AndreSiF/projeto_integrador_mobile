31c2440f09cbf517912e1aa9ffe44136f319d375
André Farias <andre.silva.farias2@gmail.com>
2025-06-05 11:14:09 -0300

    CompoForm created
    Criado um componente que serve como campo para o formulário, deve diminuir DRASTICAMENTE a quantidade de linhas usadas no código
    
    TODO: aplicar em todos os passos do formulário

---
+++ lib/pages/fields/campo_form_component.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class CampoForm extends StatelessWidget{
+  final String label;
+  final String? value;
+  final TextEditingController? controller;
+  final bool required;
+
+  const CampoForm({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.controller,
+    required this.required,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if(required){
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            labelText: label,
+            floatingLabelBehavior: FloatingLabelBehavior.auto,
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFF6F6A7E)),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFF6F6A7E), width: 2),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            labelStyle: TextStyle(color: Color(0xFF6F6A7E)),
+          ),
+          validator: (value) =>
+          value == null || value.isEmpty ? 'Campo obrigatório' : null,
+        ),
+      );
+    }
+    else{
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            labelText: label,
+            floatingLabelBehavior: FloatingLabelBehavior.auto,
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFF6F6A7E)),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFF6F6A7E), width: 2),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            labelStyle: TextStyle(color: Color(0xFF6F6A7E)),
+          ),
+        ),
+      );
+    }
+  }
+}
